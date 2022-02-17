9c7159ccd2d5cb5722a6c1560a7e5d9a2cc50fe8
EnterpriseJavaProject <85710578+EnterpriseJavaProject@users.noreply.github.com>
2022-02-17 15:02:37 +0000

    updates

--- config.php
+++ config.php
@@ -1,10 +1,14 @@
 <?php
+  
   $hostname = "sql205.unaux.com";
   $username = "unaux_28779433";
   $password = "90p9kz92";
   $dbname = "unaux_28779433_chatapp";
+  
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
+  }
 ?>
