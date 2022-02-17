e375ab87535f8a679ca5259822c1beb1edaec1be
EnterpriseJavaProject <85710578+EnterpriseJavaProject@users.noreply.github.com>
2022-02-17 15:10:55 +0000

    update in database

--- config.php
+++ config.php
@@ -1,14 +1,9 @@
 <?php
   
-  $hostname = "sql205.unaux.com";
-  $username = "unaux_28779433";
-  $password = "90p9kz92";
-  $dbname = "unaux_28779433_chatapp";
-  
+$conn = pg_connect("host=ec2-3-212-143-188.compute-1.amazonaws.com port=5432 dbname=dfmlibde0ugb8m user=zvgmbzipahfthh password=4d0f3e0f3b2857779e9a29cc9ad00865670b7970d3a8bedf8a495d7158d5440f");
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
-  
   if(!$conn){
+
     echo "Database connection error".mysqli_connect_error();
   }
 ?>
